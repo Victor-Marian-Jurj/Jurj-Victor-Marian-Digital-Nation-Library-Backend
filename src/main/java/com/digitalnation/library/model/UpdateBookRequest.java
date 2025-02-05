f8bb25c1db2fe1ef1bbaa2cdf41aff0fc9bef144
Victor Marian Jurj <victor.jurj@yahoo.com>
2025-02-05 14:21:00 +0200

    create UpdateBookRequest

---
+++ src/main/java/com/digitalnation/library/model/UpdateBookRequest.java
@@ -0,0 +1,66 @@
+package com.digitalnation.library.model;
+
+import java.time.LocalDate;
+
+public class UpdateBookRequest {
+
+    private String isbn;
+
+    private String authors;
+
+    private String title;
+
+    private String types;
+
+    private int year;
+
+    private String description;
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(String authors) {
+        this.authors = authors;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTypes() {
+        return types;
+    }
+
+    public void setTypes(String types) {
+        this.types = types;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
