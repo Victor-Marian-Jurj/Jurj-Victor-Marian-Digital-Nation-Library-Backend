3d6dddf52db7b9d1c2e639b6cf0e6f77297dfa16
Victor Marian Jurj <victor.jurj@yahoo.com>
2025-02-05 14:20:05 +0200

    create BookDto

---
+++ src/main/java/com/digitalnation/library/model/BookDto.java
@@ -0,0 +1,35 @@
+package com.digitalnation.library.model;
+
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@Data
+public class BookDto {
+
+    private Long id;
+
+    private String isbn;
+
+    private String authors;
+
+    private String title;
+
+    private String types;
+
+    private int year;
+
+    private String description;
+
+    public BookDto() {
+    }
+
+
+
+
+
+
+}
